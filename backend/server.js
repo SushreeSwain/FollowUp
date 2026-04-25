8d678e8264e0b3b51bddb581fd49704ca6dbb1c7
Sushree S Swain <sushreesswain03@gmail.com>
2026-04-25 22:10:25 +0530

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
+import 'dotenv/config'; 
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
 import cors from 'cors';
 
 import clientRoutes from './routes/ClientRoutes.js';
